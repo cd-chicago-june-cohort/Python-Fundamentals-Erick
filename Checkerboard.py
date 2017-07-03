680922eaf505a382a663053a622df78cc123d7d0
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-07-03 18:39:19 -0500

    Checkerboard

---
+++ Checkerboard.py
@@ -0,0 +1,22 @@
+def checker_board():
+    for i in range(0, 1):
+        print('*  ') * 4
+    for i in range(0, 1):
+        print(' * ') * 4
+    for i in range(0, 1):
+        print('*  ') * 4
+    for i in range(0, 1):
+        print(' * ') * 4
+    for i in range(0, 1):
+        print('*  ') * 4
+    for i in range(0, 1):
+        print(' * ') * 4
+    for i in range(0, 1):
+        print('*  ') * 4
+    for i in range(0, 1):
+        print(' * ') * 4
+    for i in range(0, 1):
+        print('*  ') * 4
+    for i in range(0, 1):
+        print(' * ') * 4
+checker_board()
