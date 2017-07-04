5e11e1b1eb3cc2c89ee51771e56c1831331d5dd4
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-07-03 20:38:05 -0500

    Made improvements

--- ScoresAndGrades.py
+++ ScoresAndGrades.py
@@ -1,18 +1,18 @@
 import random
 
 #Generates 10 random grades
-def random_grades():
+def random_grades(numOfGrades):
     print('Score and Grades')
-    for i in range(1, 10):
-        random_num = random.randint(60, 100)
+    for i in range(1, numOfGrades):
+        random_num = random.randint(60, 101)
         if(random_num > 90):
-            print('Score: ' + str(random_num) + ';  Your grade is A')
+            print('Score: ' + str(random_num) + ';   Your grade is A')
         elif(random_num <=89 and random_num >= 80):
-            print('Score: ' + str(random_num) + ';  Your grade is B')
+            print('Score: ' + str(random_num) + ';   Your grade is B')
         elif(random_num <=79 and random_num >= 70):
-            print('Score: ' + str(random_num) + ';  Your grade is C')
+            print('Score: ' + str(random_num) + ';   Your grade is C')
         elif(random_num <=69 and random_num >= 60):
-            print('Score: ' + str(random_num) + ';  Your grade is D')
+            print('Score: ' + str(random_num) + ';   Your grade is D')
     print('End of the program. Bye!')
         
-random_grades()
+random_grades(10)
