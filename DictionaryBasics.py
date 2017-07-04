19f8f1de4f96c415f36b76b6ae8676ce2f640dcc
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-07-03 22:11:29 -0500

    Dictionary Basics

---
+++ DictionaryBasics.py
@@ -0,0 +1,10 @@
+me = {
+    "name":"Erick",
+    "age":"23",
+    "countryOfBirth":"America",
+    "favoriteLanguage":"Python"
+}
+print('My name is ' + me.get("name"))
+print('My age is ' + me.get("age"))
+print('My country of birth is ' + me.get("countryOfBirth"))
+print('My favorite language is ' + me.get("favoriteLanguage"))
